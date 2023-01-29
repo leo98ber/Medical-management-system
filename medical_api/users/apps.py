a0ce76a9aaf629b46f24491548899a7bb8eab2e5
leo98ber <leonardoberoes94@gmail.com>
2023-01-29 10:07:08 -0400

    feat: add users api
    BREAKING CHANGE: it defined the users model and make migrations

---
+++ medical_api/users/apps.py
@@ -0,0 +1,12 @@
+"""Users app."""
+
+# Django
+from django.apps import AppConfig
+
+
+class UsersConfig(AppConfig):
+    """Users app config."""
+    # default_auto_field = 'django.db.models.BigAutoField'
+    name = 'users'
+    verbose_name = 'users'
+
---
+++ medical_api/users/models.py
@@ -0,0 +1,40 @@
+"""User model."""
+
+# Django
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
+
+# Utilities
+from utils.models import BaseModel
+
+
+class User(BaseModel, AbstractUser):
+    """User model.
+
+    Extend from Django's Abstract User, change the username field
+    to email and add some extra fields.
+    """
+
+    email = models.EmailField(
+        'email address',
+        unique=True,
+        error_messages={
+            'unique': 'A user with that email already exists.'
+        }
+    )
+
+    phone_regex = RegexValidator(
+        regex=r'\+?1?\d{9,15}$',
+        message="Phone number must be entered in the format: +999999999. Up to 15 digits allowed."
+    )
+
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
+
+    USERNAME_FIELD = 'email'  # Esta variable me setea al correo como parametro de logeo
+    REQUIRED_FIELDS = ['username', 'first_name',
+                       'last_name']
+
+    def __str__(self):
+        """Return username."""
+        return self.username
