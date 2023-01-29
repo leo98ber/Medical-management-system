dc7a63c3649e0159c77ab79c701b770e797f9ac7
leo98ber <leonardoberoes94@gmail.com>
2023-01-29 15:33:22 -0400

    feat: add app doctor class

---
+++ medical_api/doctors/apps.py
@@ -0,0 +1,10 @@
+"""Doctors app."""
+from django.apps import AppConfig
+
+
+class DoctorConfig(AppConfig):
+    """Doctors app config."""
+    # default_auto_field = 'django.db.models.BigAutoField'
+    name = 'doctors'
+    verbose_name = 'doctors'
+
