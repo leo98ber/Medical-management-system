6219b54ab37e98b862781252f8b58ab277ad01ea
Leonardo Beroes <leonardoberoes94@gmail.com>
2023-01-30 16:07:41 -0400

    feat: add vuex

--- medical_ui/src/main.js
+++ medical_ui/src/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+// import router from './Routes'
+import store from './store/index'
+// import axios from 'axios'
 import vuetify from './plugins/vuetify'
+
 
 Vue.config.productionTip = false
 
 new Vue({
   vuetify,
-  render: h => h(App)
+  // router,
+  render: h => h(App), store
 }).$mount('#app')
