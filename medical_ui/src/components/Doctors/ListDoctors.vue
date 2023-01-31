d823a61d876767eb9b6dd6b179ff76282bc7a1fd
leo98ber <leonardoberoes94@gmail.com>
2023-01-30 23:29:22 -0400

    feat: create ListDoctors.vue as component

---
+++ medical_ui/src/components/Doctors/ListDoctors.vue
@@ -0,0 +1,22 @@
+   <template lang="html">
+      <div class="container">
+        <div class="row">
+          <div class="col text-left">
+            <h2>
+              Lista de doctores disponibles
+            </h2>
+          </div>
+        </div>
+      </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ListDoctors',
+};
+</script>
+
+   <style lang="css" scoped>
+
+   </style>
