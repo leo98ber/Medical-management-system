4e9d3ddd1f635cda06c44c195f8fa739cfd74de0
Leonardo Beroes <leonardoberoes94@gmail.com>
2023-02-01 14:48:58 -0400

    feat: add nav bar in App.vue

---
+++ medical_ui/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+<div>
+  <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <img width="50" height="50" class="img-fluid mx-auto d-block"  src="./assets/medical_logo.jpg" alt="">
+        </div>
+        </div>
+    </div>
+
+    <b-collapse id="nav-collapse" is-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item-dropdown right>
+          <!-- Using 'button-content' slot -->
+          <template #button-content>
+            <em>User</em>
+          </template>
+          <b-dropdown-item href="#">Profile</b-dropdown-item>
+          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</div>
+
+
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
