3e1ad62f8191408183f22761b10d005bbfebfd5e
leo98ber <leonardoberoes94@gmail.com>
2023-02-02 18:54:54 -0400

    feat: add bootstrap configuration en main.js

--- medical_ui/src/main.js
+++ medical_ui/src/main.js
@@ -3,10 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
 new Vue({
   el: '#app',
   router,
