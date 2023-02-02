faa631a34236694e7dbdf47351edc097c2ed5842
leo98ber <leonardoberoes94@gmail.com>
2023-02-02 18:54:19 -0400

    feat: add routes in routers
    BREAKING CHANGE: Each path call a specific component to show data in ui

--- medical_ui/src/router/index.js
+++ medical_ui/src/router/index.js
@@ -1,15 +1,45 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import ListDoctors from '@/components/Doctors/ListDoctors.vue';
+import EditUser from "../components/Users/EditUser.vue";
+import DeleteUser from "../components/Users/DeleteUser.vue";
+import CreateUserSignUp from "../components/Users/CreateUserSignUp.vue";
+import Login from "../components/Users/Login.vue";
+
+
 
 Vue.use(Router)
 
-export default new Router({
+export default new Router(
+
+  {
+  mode: "history",
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      name: 'ListDoctors',
+      component: ListDoctors
+    },
+    {
+      path: '/users/edit',
+      name: 'EditUser',
+      component: EditUser
+    },
+    {
+      path: '/users/delete',
+      name: 'DeleteUser',
+      component: DeleteUser
+    },
+    {
+      path: '/signup',
+      name: 'CreateUserSignUp',
+      component: CreateUserSignUp
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+
   ]
 })
