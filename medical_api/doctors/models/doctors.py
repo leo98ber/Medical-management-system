a696c1b3123f01855e614550b7c53b57d4de673d
Leonardo Beroes <leonardoberoes94@gmail.com>
2023-02-03 09:29:01 -0400

    refactor: change doctors migrations

--- medical_api/doctors/models/doctors.py
+++ medical_api/doctors/models/doctors.py
@@ -10,15 +10,10 @@ from utils.models import BaseModel
 
 class Doctor(BaseModel):
     first_name = models.CharField(max_length=50)
-
     last_name = models.CharField(max_length=50)
-
     biography = models.TextField(max_length=500, blank=True)
-
     availability = models.DurationField(timedelta(hours=0))
-
     major = models.OneToOneField('doctors.Major', on_delete=models.CASCADE)
-
     centers = models.ManyToManyField('doctors.Center')
 
     def __str__(self):
