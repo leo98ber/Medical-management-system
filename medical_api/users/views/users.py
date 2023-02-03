b7a87e463bc24a400f57e63ff71be1ad3fd823c8
Leonardo Beroes <leonardoberoes94@gmail.com>
2023-02-03 10:15:09 -0400

    refactor: remove unnecessary methods

--- medical_api/users/views/users.py
+++ medical_api/users/views/users.py
@@ -26,26 +26,6 @@ class UserViewSet(mixins.RetrieveModelMixin,
     permission_classes = [IsAuthenticated]
     lookup_field = 'username'
 
-    def list(self, request, *args, **kwargs):
-        print(request.headers)
-        print(request.user.is_authenticated)
-        queryset = self.filter_queryset(self.get_queryset())
-
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
-
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
-
-    def retrieve(self, request, *args, **kwargs):
-        print(request.headers)
-        print(request.user.is_authenticated)
-        instance = self.get_object()
-        serializer = self.get_serializer(instance)
-        return Response(serializer.data)
-
     def get_permissions(self):
         if self.action in ['retrieve', 'update', 'list', 'delete']:
             permissions = [IsAuthenticated]
